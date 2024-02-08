09d502ebcd9e2393390168c167a008e14e26354c
jpdelgad0 <jpdelgado300@gmail.com>
2024-02-08 11:23:59 -0500

    Initial Commit

---
+++ 1Football: live scores/_Football__live_scoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Football__live_scoresApp.swift
+//  1Football: live scores
+//
+//  Created by Juanpablo Delgado on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct _Football__live_scoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
